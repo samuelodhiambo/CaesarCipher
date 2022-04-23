81fa7ee7efccb9d39a8b3570f78bf2cbca817af4
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-23 10:24:04 +0300

    patch: It will add logic to handle cipher decoding

--- src/main/java/com/moringajava/DeCipher.java
+++ src/main/java/com/moringajava/DeCipher.java
@@ -5,6 +5,20 @@ public class DeCipher {
 
     public static String decode(String message, int offset) {
         String plainText = "";
+        message = message.toLowerCase();
+        for (int i=0; i<message.length(); i++) {
+            if (message.charAt(i) != ' ') {
+                int currentChar = alphabet.indexOf(message.charAt(i));
+                int newIndex = currentChar + offset;
+                if (newIndex < 0) {
+                    newIndex = 26 + newIndex;
+                }
+                char cipherChar = alphabet.charAt(newIndex);
+                plainText = plainText + cipherChar;
+            } else {
+                plainText = plainText + " ";
+            }
+        }
         return plainText;
     }
 }
