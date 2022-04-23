40ada7acfc6abcd025b9b652c8c2da4a513f4467
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-23 10:53:53 +0300

    fix: It will handle user input and display both ciphertext and plain message

--- src/main/java/com/moringajava/Main.java
+++ src/main/java/com/moringajava/Main.java
@@ -1,7 +1,18 @@
 package com.moringajava;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        Cipher cipher = new Cipher();
+        DeCipher deCipher = new DeCipher();
+        System.out.println("Enter your message >> ");
+        String message = scan.nextLine();
+        System.out.println("Enter key >> ");
+        int key = scan.nextInt();
+        String cipherMessage = cipher.encode(message, key);
+        String plainMessage = deCipher.decode(cipherMessage, key);
+        System.out.println("The ciphered message is: " + cipherMessage + ", and the plain message is: " + plainMessage);
     }
 }
