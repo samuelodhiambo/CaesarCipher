81d2c84a92e75fbb340f620b593554acab53e6d2
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-23 10:04:56 +0300

    patch: It will add global alphabet variable and declare cipher/decipher methods

---
+++ src/main/java/com/moringajava/Cipher.java
@@ -0,0 +1,10 @@
+package com.moringajava;
+
+public class Cipher {
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encode(String message, int offset) {
+        String cipherText = "";
+        return cipherText;
+    }
+}
---
+++ src/main/java/com/moringajava/DeCipher.java
@@ -0,0 +1,10 @@
+package com.moringajava;
+
+public class DeCipher {
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String decode(String message, int offset) {
+        String plainText = "";
+        return plainText;
+    }
+}
