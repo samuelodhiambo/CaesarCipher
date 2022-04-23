4450144c39dc558c911d00a2a9d01eb0bb13acda
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-23 10:21:42 +0300

    patch: It will add logic to handle message encoding

--- src/main/java/com/moringajava/Cipher.java
+++ src/main/java/com/moringajava/Cipher.java
@@ -5,6 +5,20 @@ public class Cipher {
 
     public static String encode(String message, int offset) {
         String cipherText = "";
+        message = message.toLowerCase();
+        for (int i=0; i<message.length(); i++) {
+            if (message.charAt(i) != ' ') {
+                int currentChar = alphabet.indexOf(message.charAt(i));
+                int newIndex = currentChar + offset;
+                if (newIndex > 25) {
+                    newIndex = newIndex - 26;
+                }
+                char cipherChar = alphabet.charAt(newIndex);
+                cipherText = cipherText + cipherChar;
+            } else {
+                cipherText = cipherText + " ";
+            }
+        }
         return cipherText;
     }
 }
