7b48a022501d0485e04db10e053202030f898cd8
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-23 09:44:00 +0300

    patch: It will add hello world output to Main class

--- src/main/java/com/moringajava/Main.java
+++ src/main/java/com/moringajava/Main.java
@@ -1,4 +1,6 @@
-package com.moringajava;public class Main {
+package com.moringajava;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
