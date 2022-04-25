db7c7a038716944c561b572f986d76eb2405abf0
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-25 10:16:16 +0300

    patch: It will change decipher class instansiation

--- src/test/java/com/moringajava/DeCipherTest.java
+++ src/test/java/com/moringajava/DeCipherTest.java
@@ -7,9 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class DeCipherTest {
     @Test
     public void run_decipher_test(){
-        DeCipher deCipher = new DeCipher();
         String input = "nby kocwe vliqh zir dogjm ipyl nby futs xia";
-        String plainText = deCipher.decode(input, 20);
+        String plainText = DeCipher.decode(input, 20);
         String expectedOutput = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG".toLowerCase();
         assertEquals(expectedOutput, plainText);
     }
