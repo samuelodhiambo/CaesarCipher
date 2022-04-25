13d12e9ba2992d48d5064db4a1ee375c8ea6e47e
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-25 10:16:03 +0300

    patch: It will change cipher class instansiation

--- src/test/java/com/moringajava/CipherTest.java
+++ src/test/java/com/moringajava/CipherTest.java
@@ -7,9 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class CipherTest {
     @Test
     public void run_cipher_test(){
-        Cipher cipher = new Cipher();
         String input = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
-        String cipherText = cipher.encode(input, 20);
+        String cipherText = Cipher.encode(input, 20);
         String expectedOutput = "nby kocwe vliqh zir dogjm ipyl nby futs xia";
         assertEquals(expectedOutput, cipherText);
     }
